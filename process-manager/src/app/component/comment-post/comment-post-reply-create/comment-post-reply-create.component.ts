dc2a1d007f6ad5f12f2592260c294becb79f7f82
quoctrung1012 <quoctrung1012@gmail.com>
2021-03-18 21:43:31 +0700

    trung up bai

---
+++ process-manager/src/app/component/comment-post/comment-post-reply-create/comment-post-reply-create.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-comment-post-reply-create',
+  templateUrl: './comment-post-reply-create.component.html',
+  styleUrls: ['./comment-post-reply-create.component.scss']
+})
+export class CommentPostReplyCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
