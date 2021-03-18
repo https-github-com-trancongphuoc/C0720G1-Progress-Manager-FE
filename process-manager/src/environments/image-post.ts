6835444d63b6eb13a5fb2a6287facd04b7c54651
luyen <luyen2011@gmail.com>
2021-03-18 17:23:29 +0700

    luyen up

---
+++ process-manager/src/environments/image-post.ts
@@ -0,0 +1,12 @@
+export const imagePost = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAPsEKjd1EB6VZkkC93pD4RWyGfd06yg1A",
+    authDomain: "project-dating-c8c29.firebaseapp.com",
+    projectId: "project-dating-c8c29",
+    storageBucket: "project-dating-c8c29.appspot.com",
+    messagingSenderId: "597750326479",
+    appId: "1:597750326479:web:97e5e050211103ea5443ba",
+    measurementId: "G-5RR9R7CTYQ"
+  }
+};
